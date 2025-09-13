d81345832f49929c975524d9d6a1ace9671b62cd
Gregorio Firmani <gregoriofirmani@gmail.com>
2025-09-12 21:33:07 -0300

    Planteamiento de sidebar y cambios en el css

--- task-dashboard/src/App.tsx
+++ task-dashboard/src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 import { useState } from 'react';
 import { TaskCard } from './components/TaskCard';
-import { type Task } from './types/Task';
 import {tasks} from './data/tasks';
 import { Divide } from 'lucide-react';
 
@@ -12,21 +11,28 @@ function App() {
   ));
 
   const taskBoard = (
-      <div className="bg-slate-900 p-10 flex justify-center items-center max-w-7xl">
-        <div className="flex flex-row flex-wrap gap-2.5">
-          {taskList}
+      <div className="bg-slate-900 w-12/12 flex flex-row rounded-lg m-5">
+        <div className="bg-red-500 w-1/5 p-10">
+          <h2 className="text-2xl font-semibold text-white text-center p-3">Sidebar</h2>
+        </div>
+        <div className='p-10 flex justify-center items-center'>
+          <div className="flex flex-row flex-wrap gap-2.5 justify-center">
+            {taskList}
+          </div>
         </div>
       </div>
   );
 
   return (
-    <div>
+    <div className='min-h-screen bg-gradient-to-b from-slate-800 to-slate-900'>
         <header>
           <h1 className="text-4xl font-bold text-white text-center p-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
             Personal Dashboard
           </h1>
         </header>
-        {taskBoard}
+        <div className ="flex justify-center items-center">
+          {taskBoard}
+        </div>
     </div>
   );
 }
